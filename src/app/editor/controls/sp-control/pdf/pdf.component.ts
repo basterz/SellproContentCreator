06286a1643e0bd5a4dc6964c64c0e0b201ba5738
basterz <cecohim4ev@gmail.com>
2019-09-30 17:07:32 +0300

    First commit

--- src/app/editor/controls/sp-control/pdf/pdf.component.ts
+++ src/app/editor/controls/sp-control/pdf/pdf.component.ts
@@ -1,21 +1,19 @@
 import { Component, OnInit } from '@angular/core';
 import {ControlComponent} from '../control.component';
 import {PdfViewModel} from './pdf-view-model';
-import {PdfSettingsComponent} from './pdf-settings/pdf-settings.component';
 import {ControlLayer} from '../control-layer';
-import {ImageViewModel} from '../image/image-view-model';
 
 @Component({
   selector: 'app-pdf',
   templateUrl: './pdf.component.html',
   styleUrls: ['./pdf.component.scss']
 })
+
 export class PdfComponent extends ControlComponent implements OnInit {
   viewModel: PdfViewModel;
   constructor() {
     super();
     this.viewModel = new PdfViewModel();
-
   }
 
   ngOnInit() {
@@ -25,10 +23,5 @@ export class PdfComponent extends ControlComponent implements OnInit {
     if (files != null) {
       layer.fileUrl = URL.createObjectURL(files[0]);
     }
-  }
-
-  DownloadFile(fileUrl: string) {
-    console.log(fileUrl);
-    window.open(fileUrl);
   }
 }
